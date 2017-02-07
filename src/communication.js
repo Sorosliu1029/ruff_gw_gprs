6deaa7c774fb3ca07dcde58f9db10e1f7ed2b5af
Soros Liu <soros.liu1029@gmail.com>
2017-02-07 17:06:06 +0800

    refactor code

---
+++ src/communication.js
@@ -0,0 +1,119 @@
+/*!
+ * Copyright (c) 2017 Nanchao Inc.
+ * All rights reserved.
+ */
+
+'use strict';
+
+var EventEmitter = require('events');
+var util = require('util');
+var assert = require('assert');
+var Queue = require('ruff-async').Queue;
+var ReadStreaming = require('./read-streaming');
+
+var State = {
+  idle: 0,
+  waitingResponse: 1
+};
+
+function Communication(port) {
+  EventEmitter.call(this);
+  this._cs = State.idle;
+  this._port = port;
+  this._cmdQueue = new Queue(this._processCmd);
+  this._pendingData = new Buffer(0);
+  this._parseResponse = null;
+
+  this._readStream = new ReadStreaming(port);
+  this._readStream.on('data', this._parseData.bind(this));
+  this._readStream.on('error', function () {
+    throw new Error('UART is crashed');
+  });
+  this._readStream.start();
+}
+
+util.inherits(Communication, EventEmitter);
+
+Communication.prototype.sendRawData = function (data, callback) {
+  this._port.write(data, function (error) {
+    if (error) {
+      callback && callback(error);
+      return;
+    }
+    callback && callback.apply(undefined, arguments);
+  });
+};
+
+Communication.prototype.pushCmd = function (cmdOptions, callback) {
+  assert(cmdOptions.requestData);
+  assert(cmdOptions.responseTimeout);
+  assert(typeof cmdOptions.parseResponse === 'function');
+
+  this._cmdQueue.push(this, [cmdOptions], callback);
+}
+
+Communication.prototype._getResponse = function (timeout, callback) {
+  this._cs = State.waitingResponse;
+  var that = this;
+
+  var timerHandle = setTimeout(responseDoneCleanup.bind(undefined, new Error('Response timeout')), timeout);
+
+  var onResponseDone = responseDoneCleanup.bind(undefined, undefined);
+  this.on('responseDone', onResponseDone);
+
+  function responseDoneCleanup(error, responseData) {
+    clearTimeout(timerHandle);
+    that.removeListener('responseDone', onResponseDone);
+    that._cs = State.idle;
+    that._pendingData = new Buffer(0);
+    callback(error, responseData);
+  }
+};
+
+Communication.prototype._processCmd = function (cmdOptions, callback) {
+  assert(this._cs === State.idle);
+  var cmdData = cmdOptions.requestData;
+  var cmdTimeout = cmdOptions.responseTimeout;
+
+  this._parseResponse = cmdOptions.parseResponse;
+
+  this._getResponse(cmdTimeout, invokeCallbackOnce);
+
+  this._port.write(cmdData, function (error) {
+    if (error) {
+      invokeCallbackOnce(error);
+      return;
+    }
+  });
+
+  var callbackInvoked = false;
+
+  function invokeCallbackOnce() {
+    if (!callbackInvoked) {
+      callbackInvoked = true;
+      callback && callback.apply(undefined, arguments);
+    }
+  }
+};
+
+Communication.prototype._consume = function (length) {
+  this._pendingData = this._pendingData.slice(length);
+};
+
+Communication.prototype._parseData = function (data) {
+  if (this._cs === State.idle) {
+    this._pendingData = new Buffer(0);
+    return;
+  }
+
+  this._pendingData = Buffer.concat([this._pendingData, data]);
+  if (this._cs === State.waitingResponse) {
+    var response = this._parseResponse(this._pendingData);
+    if (response.index[1] > 0) {
+      this._consume(response.index[0] + response.index[1]);
+      this.emit('responseDone', response.valid);
+    }
+  }
+};
+
+module.exports = Communication;
---
+++ src/read-streaming.js
@@ -0,0 +1,35 @@
+/*!
+ * Copyright (c) 2017 Nanchao Inc.
+ * All rights reserved.
+ */
+
+'use strict';
+
+var util = require('util');
+var EventEmitter = require('events');
+
+function ReadStreaming(obj) {
+  EventEmitter.call(this);
+  this._read = obj.read.bind(obj);
+}
+
+util.inherits(ReadStreaming, EventEmitter);
+
+ReadStreaming.prototype.start = function () {
+  var that = this;
+
+  readNext();
+
+  function readNext() {
+    that._read(function (err, data) {
+      if (err) {
+        that.emit('error', err);
+      } else {
+        that.emit('data', data);
+        readNext();
+      }
+    });
+  }
+};
+
+module.exports = ReadStreaming;
